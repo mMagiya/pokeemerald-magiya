49d882f9540f789429607bddcfb5340521e82b74
Magiya <46344714+mMagiya@users.noreply.github.com>
2024-04-18 00:12:26 -0600

    add fake rtc system for better time based things

--- include/rtc.h
+++ include/rtc.h
@@ -119,4 +119,7 @@ void CalcTimeDifference(struct Time *result, struct Time *t1, struct Time *t2);
 u32 RtcGetMinuteCount(void);
 u32 RtcGetLocalDayCount(void);
 
+void RtcAdvanceTime(u32 hours, u32 minutes, u32 seconds);
+void RtcAdvanceTimeTo(u32 hours, u32 minutes, u32 seconds);
+
 #endif // GUARD_RTC_UTIL_H
