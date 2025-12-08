4d64b6be3bb91816f210a4d8a5cbb30a4bb5a0ca
Magiya <46344714+mMagiya@users.noreply.github.com>
2025-12-08 13:24:31 -0700

    slight map rework, beginning of weather and time based encounters

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -264,8 +264,9 @@
 #define VAR_ROXANNE_CALL_STEP_COUNTER                    0x40F4
 #define VAR_SCOTT_BF_CALL_STEP_COUNTER                   0x40F5
 #define VAR_RIVAL_RAYQUAZA_CALL_STEP_COUNTER             0x40F6
-#define VAR_UNUSED_0x40F7                                0x40F7 // Unused Var
-#define VAR_UNUSED_0x40F8                                0x40F8 // Unused Var
+// Begin Magiya Custom Additions
+#define VAR_WEATHER_CURRENT                              0x40F7 // Current state of the Weather system
+#define VAR_WEATHER_PREVIOUS                             0x40F8 // Unused Var - previous state of the Weather system
 #define VAR_UNUSED_0x40F9                                0x40F9 // Unused Var
 #define VAR_UNUSED_0x40FA                                0x40FA // Unused Var
 #define VAR_UNUSED_0x40FB                                0x40FB // Unused Var
@@ -273,6 +274,7 @@
 #define VAR_UNUSED_0x40FD                                0x40FD // Unused Var
 #define VAR_UNUSED_0x40FE                                0x40FE // Unused Var
 #define VAR_UNUSED_0x40FF                                0x40FF // Unused Var
+// End Magiya Custom Additions
 
 #define VARS_END                                         0x40FF
 #define VARS_COUNT                                       (VARS_END - VARS_START + 1)
